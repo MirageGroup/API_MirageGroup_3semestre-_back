0067dc335a891812e6b49823ea16142faf83f2cc
pedro11pucci <pedro11pucci@gmail.com>
2023-09-12 10:33:42 -0300

    Setting up database

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import Express from "express";
+import appDataSource from "./infra/data-source";
 
 const app = Express()
 require('dotenv').config()
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`)
+})
+
+appDataSource.initialize().then(() => {
+    console.log("Database initialized succesfully")
 })
 
 app.get('/', (req, res) => {
