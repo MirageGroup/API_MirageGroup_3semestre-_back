f2197f25d5466caca2d4e33f020585b6e29f2f8b
pedro11pucci <pedro11pucci@gmail.com>
2023-09-19 20:41:07 -0300

    feat: create new process

--- src/routes/process.router.ts
+++ src/routes/process.router.ts
@@ -1,6 +1,16 @@
 import { Router } from "express";
-import processController from "../controllers/processController";
+import Process from '../infra/entities/process.entity';
+import ProcessServices from '../services/process.services';
+import appDataSource from '../infra/data-source';
+import ProcessController from '../controllers/process.controller';
 
-const process = Router()
+require('dotenv').config()
 
-process.get('/')
+export const process = Router()
+
+const service = new ProcessServices(appDataSource.getRepository(Process))
+const controller = new ProcessController(service)
+
+process.post('/create', async (req, res) => {
+    await controller.createProcess(req, res)
+})
--- src/services/process.services.ts
+++ src/services/process.services.ts
@@ -1,5 +1,15 @@
-export default class ProcessService {
+import { Repository } from 'typeorm';
+import appDataSource from '../infra/data-source';
+import Process from '../infra/entities/process.entity';
 
-    
+export default class ProcessServices{
 
+    public constructor(
+        private readonly processRepository: Repository<Process>
+    ){}
+   
+    public async createProcess(process: Process): Promise<Process> {
+        const newProcess = await this.processRepository.save(process)
+        return newProcess
+    }
 }
