1ad3db3a36709dfde4690d6caf9d31ed781a3ace
pedro11pucci <pedro11pucci@gmail.com>
2023-11-02 14:09:12 -0300

    refactor initial role data migration

--- src/infra/migrations/1698936860621-RoleInitialData.ts
+++ src/infra/migrations/1698936860621-RoleInitialData.ts
@@ -5,9 +5,9 @@ export class RoleInitialData1698936860621 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `INSERT INTO role(id, name, create_user, create_process, create_task, create_evidence, view_user, view_process, view_task, view_evidence) VALUES 
-            (1, "tech-lead", false, false, true, true, false, true, true, true),
-            (2, "c_level", false, false, false, false, true, true, true, true),
-            (3, "director", true, true, true, true, true, true, true, true ),
+            (1, "c_level", false, false, false, false, true, true, true, true),
+            (2, "director", true, true, true, true, true, true, true, true ),
+            (3, "tech-lead", false, false, true, true, false, true, true, true),
             (4, "colaborator", false, false, false, true, false, true, true, true);`
         )
     }
