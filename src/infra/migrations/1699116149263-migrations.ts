4a79a0231052292bbb8c9a39c0fc4fd6627251ef
pedro11pucci <pedro11pucci@gmail.com>
2023-11-04 13:51:31 -0300

    create default user migration

---
+++ src/infra/migrations/1699116149263-migrations.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+import bcrypt from 'bcrypt'
+
+export class Migrations1699116149263 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `INSERT INTO user(name, email, password, phone, cpf, roleId) VALUES 
+            ("Usuário", "usuario@email.com", "${await bcrypt.hash("senha123", 10)}", "+11 (11) 11111-1111", "111.111.111-11", 1)`
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `DELETE FROM user WHERE name = "Usuário"`
+        )
+    }
+
+}
