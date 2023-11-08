ba522c6ab84358f9cd563b8d0e10cccdb710eb4f
pedro11pucci <pedro11pucci@gmail.com>
2023-11-08 12:01:32 -0300

    update initial user migration

--- src/infra/migrations/1699116149263-migrations.ts
+++ src/infra/migrations/1699116149263-migrations.ts
@@ -6,7 +6,7 @@ export class Migrations1699116149263 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `INSERT INTO user(name, email, password, phone, cpf, roleId) VALUES 
-            ("Usuário", "usuario@email.com", "${await bcrypt.hash("senha123", 10)}", "+11 (11) 11111-1111", "111.111.111-11", 1)`
+            ("Usuário", "usuario@email.com", "${await bcrypt.hash("senha123", 10)}", "+11 (11) 11111-1111", "111.111.111-11", 2)`
         )
     }
 
